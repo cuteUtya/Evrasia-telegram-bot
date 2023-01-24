841eb6ff466ca66c01ac235086e1820615ffcc1d
Tim-dev-hub <61709763+cuteUtya@users.noreply.github.com>
2023-01-24 09:08:13 +0200

    write basic telegram bot part

--- index.ts
+++ index.ts
@@ -1,14 +1,5 @@
-import {request} from "./src/evrasia-request";
-import { getRandomUserAgent } from "./src/user-agents";
+import { run } from "./src/botEntry";
 import { UserDatabase } from "./src/user-database";
 
 UserDatabase.init();
-UserDatabase.getUser(5).then((e) => console.log(e));
-
-
-/*
-request({link: 'https://evrasia.spb.ru/signin/', headers: {
-    'user-agent': getRandomUserAgent(),
-}}).then((r) => {
-    console.log(r.headers);
-});*/
+run();
---
+++ src/botEntry.ts
@@ -0,0 +1,47 @@
+import TelegramBot from "node-telegram-bot-api";
+import { UserDatabase } from "./user-database";
+
+export function run() {
+    const token = '5847860544:AAFBsw2RNB0k5D-hyOEuiqtrEFOAIG6u3mU';
+    const bot = new TelegramBot(token, { polling: true });
+
+    bot.onText(/\/start/, async (m) => {
+        if(await UserDatabase.getUser(m.from.id) == null) {
+            bot.sendMessage(m.chat.id, 'Отправьте команду /login для авторизации');
+        } else {
+            bot.sendMessage(m.chat.id, 'Ви уже авторизаванный пользователь. Чтобы войти в другой аккаунт отправьте команду /logout, а затем /login');
+        }
+    });
+
+
+    var loginRequests: Array<loginRequest> = [];
+    bot.onText(/\/login/, (m) => {
+        bot.sendMessage(m.chat.id, 'Введите номер телефона, к которому привязан аккаунт в формате +7XXXXXXXXXXX');
+        loginRequests.push(new loginRequest(m.from.id));
+    });
+
+    bot.onText(/\+[0-9]{11}/, (m) => {
+        var r = loginRequests.find((d) => d.id == m.from.id); 
+        if(r != undefined) {
+            r.phone = m.text;
+            bot.sendMessage(r.id, 'Теперь ввидете ваш пароль');
+        }
+    });
+
+    bot.onText(/.*/, (m) => {
+        var r = loginRequests.find((d) => d.id == m.from.id); 
+        if(r != undefined) {
+            //TODO: check password
+        }
+    })
+}
+
+class loginRequest {
+    id: number;
+    phone?: string;
+    password?: string;
+    
+    constructor(id: number) {
+        this.id = id;
+    }
+}
