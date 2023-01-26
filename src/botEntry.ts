ea63fc96cc444b68e6f1ca85f2cdd9d029346768
Tim-dev-hub <61709763+cuteUtya@users.noreply.github.com>
2023-01-26 20:46:19 +0200

    Add ability to show adresses

--- src/botEntry.ts
+++ src/botEntry.ts
@@ -1,5 +1,5 @@
 import TelegramBot from "node-telegram-bot-api";
-import { EvrasiaApi } from "./evrasia-api";
+import { EvrasiaApi, RestaurantAdress } from "./evrasia-api";
 import { getRandomUserAgent } from "./user-agents";
 import { UserDatabase } from "./user-database";
 
@@ -8,23 +8,88 @@ export function run() {
     const bot = new TelegramBot(token, { polling: true });
 
     bot.onText(/\/start/, async (m) => {
-        if(await UserDatabase.getUser(m.from.id) == null) {
-            bot.sendMessage(m.chat.id, 'Отправьте команду /login для авторизации');
+        if (await UserDatabase.getUser(m.from.id) == null) {
+            await bot.sendMessage(m.chat.id, 'Отправьте команду /login для авторизации');
         } else {
-            bot.sendMessage(m.chat.id, 'Ви уже авторизаванный пользователь. Чтобы войти в другой аккаунт отправьте команду /logout, а затем /login');
+            await bot.sendMessage(m.chat.id, 'Ви уже авторизаванный пользователь. Чтобы войти в другой аккаунт отправьте команду /logout, а затем /login');
         }
     });
 
 
+    function findUserInLoginRequest(userId: number): loginRequest {
+        var r = loginRequests.find((d) => d.id == userId);
+        if (r) return null;
+        return r;
+    }
+
+
     var loginRequests: Array<loginRequest> = [];
-    bot.onText(/\/login/, (m) => {
-        bot.sendMessage(m.chat.id, 'Введите номер телефона, к которому привязан аккаунт в формате +7XXXXXXXXXXX');
-        loginRequests.push(new loginRequest(m.from.id));
+    bot.onText(/\/login/, async (m) => {
+        var usr = await UserDatabase.getUser(m.from.id);
+        var r = findUserInLoginRequest(m.from.id) == undefined;
+        if (usr == undefined && r) {
+            await bot.sendMessage(m.chat.id, 'Введите номер телефона, к которому привязан аккаунт в формате +7XXXXXXXXXXX');
+            loginRequests.push(new loginRequest(m.from.id));
+        } else if (r == undefined && usr?.cookies != '') {
+            await bot.sendMessage(m.chat.id, 'Вы уже авторизованы');
+        } else {
+            await bot.sendMessage(m.chat.id, 'Процесс авторизации уже запущен, следуйте иструкциям выше');
+        }
     });
 
+
+    var bot_adresses: RestaurantAdress[] = [];
+
+    bot.on('callback_query', async (q) => {
+        console.log(q.data);
+        var adressQuery = /getCode#(\d*)#(\d*)/.exec(q.data);
+
+        console.log(adressQuery.length);
+
+        if (adressQuery.length == 3) {
+            var code = parseInt(adressQuery[1]);
+            var user = adressQuery[2];
+
+            console.log('here');
+
+            try {
+                await bot.deleteMessage(q.message.chat.id, q.message.message_id.toString());
+                await bot.answerCallbackQuery(q.id, {});
+                await bot.sendMessage(q.from.id, 'Выбран ресторан: ' + bot_adresses.find((e) => e.index == code).name);
+            } catch (e) {
+                //query can be just too old so ignore it 
+            }
+        }
+    });
+
+    bot.onText(/\/getcode/, async (m) => {
+        var user = await UserDatabase.getUser(m.from.id);
+        if (user != undefined) {
+            var adresess = await EvrasiaApi.GetAdresess(user);
+            if (adresess.ok && adresess.result != undefined) {
+                //TODO change this logic if users can have diff adresses 
+                bot_adresses = adresess.result;
+                bot.sendMessage(m.from.id, 'Выберите адрес', {
+                    reply_markup: {
+                        inline_keyboard: adresess.result.map((e) => {
+                            return [{
+                                text: e.name,
+                                callback_data: `getCode#${e.index}#${m.from.id}`,
+                            }]
+                        })
+                    }
+                });
+            } else {
+                //TODO hold error
+            }
+        } else {
+            //todo user should login
+        }
+    })
+
     bot.onText(/\+[0-9]{11}/, (m) => {
-        var r = loginRequests.find((d) => d.id == m.from.id); 
-        if(r != undefined) {
+        var r = findUserInLoginRequest(m.from.id);
+        if (r != undefined) {
             r.phone = m.text;
             r.phoneMessageId = m.message_id;
             bot.sendMessage(r.id, 'Теперь ввидете ваш пароль');
@@ -32,42 +97,41 @@ export function run() {
     });
 
     bot.onText(/\/stopword/, (m) => {
-        var r = loginRequests.find((d) => d.id == m.from.id);
+        var r = findUserInLoginRequest(m.from.id);
 
-        if(r != undefined) {
-            //TODO 
-            //loginRequests.
+        if (r != undefined) {
+            loginRequests.splice(loginRequests.indexOf(r), 1);
+            bot.sendMessage(m.from.id, 'Процесс остановлен. Введите команду /login повторно')
         }
     });
 
     bot.onText(/.*/, async (m) => {
-        var r = loginRequests.find((d) => d.id == m.from.id); 
-        if(r != undefined) {
+        var r = findUserInLoginRequest(m.from.id);
+        if (r != undefined) {
             console.log(r);
-            if(r.phone == undefined && m.text != '/login') {
+            if (r.phone == undefined && m.text != '/login') {
                 bot.sendMessage(m.chat.id, 'Неправильный формат номера');
             } else if (r.phone != undefined && r.phoneMessageId != m.message_id) {
                 var agent = getRandomUserAgent();
                 r.password = m.text;
-                await UserDatabase.writeUser({
-                    id: m.from.id,
-                    cookies: '',
-                    isAdmin: false,
-                    userAgent: agent,
-                });
 
                 var result = await EvrasiaApi.Login(r.phone, r.password, agent);
-                
-                if(result.ok) {
-                    var usr = await UserDatabase.getUser(m.from.id);
-                    usr.cookies = result.result;
-                    await UserDatabase.editUser(usr);
-                    // do a shit
+
+                if (result.ok) {
+                    bot.sendMessage(m.chat.id, 'Теперь вы авторизованы');
+                    console.log('write user');
+                    await UserDatabase.writeUser({
+                        id: m.from.id,
+                        cookies: JSON.stringify(result.result),
+                        isAdmin: false,
+                        userAgent: agent,
+                    });
+                    //await UserDatabase.editUser(usr);
+                    loginRequests.splice(loginRequests.indexOf(r), 1);
                 } else {
-                    bot.sendMessage(m.from.id, 'Неправильный пароль или логин. Введите правильный пароль или отправьте команду /stopword чтобы остановить авторизацию')
+                    bot.sendMessage(m.from.id, 'Неправильный пароль или логин. Введите правильный пароль или отправьте команду /stopword чтобы повторить авторизацию')
                 }
             }
-            //TODO: check password
         }
     })
 }
@@ -77,7 +141,7 @@ class loginRequest {
     phone?: string;
     phoneMessageId: number;
     password?: string;
-    
+
     constructor(id: number) {
         this.id = id;
     }
--- src/evrasia-api.ts
+++ src/evrasia-api.ts
@@ -2,13 +2,20 @@ import { request } from "./evrasia-request";
 import fs from 'fs';
 
 export class EvrasiaApi {
-    static async Login(login: string, password: string, userAgent: string): Promise<RequestResult<string>> {
+    static cutCookie(cookie: string): string {
+        if(cookie.includes(';')) return cookie.split(';')[0];
+        return cookie;
+    }
 
+    static glueCookie(cookies: string[]) : string {
+        return cookies.join('; ');
+    }
+    
+    static async Login(login: string, password: string, userAgent: string): Promise<RequestResult<string[]>> {
         var firstOpen = await request({
             link: 'https://evrasia.spb.ru/signin/',
             headers: {
                 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                //'accept-encoding': 'deflate, br',
                 'accept-language': 'ru',
                 'dnt': 1,
                 'sec-fetch-dest': 'document',
@@ -20,63 +27,21 @@ export class EvrasiaApi {
             }
         });
 
-        var PHPSESSID = firstOpen.headers['set-cookie'][0].split(';')[0];
-        var BITRIX_SM_SALE_UID = firstOpen.headers['set-cookie'][1].split(';')[0];
-
-        /*
-        var sessid = /\'bitrix_sessid\':\'(.*)\'/.exec(firstOpen.body)[1];
-
-        var BITRIX_CONVERSION_CONTEXT_s1 = (await request({
-            link: `https://evrasia.spb.ru/bitrix/tools/conversion/ajax_counter.php?SITE_ID=s1&sessid=${sessid}&HTTP_REFERER=`,
-            method: 'POST',
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-                'cookie': [PHPSESSID, BITRIX_SM_SALE_UID].join('; '),
-                'user-agent': userAgent,
-            }
-        })).headers['set-cookie'][0].split(';')[0];*/
-
-        /*var BX_USER_ID = (await request({
-            link: 'https://bitrix.info/ba.js',
-            headers: {
-                'user-agent': userAgent, 
-            }
-        })).headers['set-cookie'][0].split(';')[0];
-
-        BX_USER_ID = BX_USER_ID.split('=')[0].toUpperCase() + '=' + BX_USER_ID.split('=')[1];*/
-
-        //var CHOOSED_RESTAURANT_EVERYTIME = 'CHOOSED_RESTAURANT_EVERYTIME=true';
+        var PHPSESSID = this.cutCookie(firstOpen.headers['set-cookie'][0]);
+        var BITRIX_SM_SALE_UID = this.cutCookie(firstOpen.headers['set-cookie'][1]);
 
 
-        var cookieString = [PHPSESSID,
-            BITRIX_SM_SALE_UID
-        ].join('; ')
-
-        function matchAll(regex, value): string[] {
-            var re = regex;
-            var s = value;
-            var m;
-
-            var r = [];
-
-            do {
-                m = re.exec(s);
-                if (m) {
-                    r = [...r, m[1]];
-                }
-            } while (m);
-
-            return r;
-        }
+        var cookieString = this.glueCookie([PHPSESSID,BITRIX_SM_SALE_UID]);
 
 
+        /* no sense 
         var cssResources = matchAll(/<link href=\"(.*) type=\"text\/css\"/g, firstOpen.body);
-        for(var c in cssResources) {
+        for (var c in cssResources) {
             console.log('fake loading on server: ' + cssResources[c]);
             await request({
                 link: 'https://evrasia.spb.ru/' + cssResources[c],
                 headers: {
-                    'accept': 'text/css,*/*;q=0.1',
+                    'accept': 'text/css,/*;q=0.1',
                     'accept-language': 'ru',
                     'cookie': cookieString,
                     'dnt': 1,
@@ -88,183 +53,84 @@ export class EvrasiaApi {
                     'user-agent': userAgent,
                 }
             })
+        }*/
+
+        var formattedPhone = `+${login.substring(1, 2)}(${login.substring(2, 5)})${login.substring(5, 8)}-${login.substring(8, 10)}-${login.substring(10, 12)}`;
+        var signInPath = `USER_LOGIN=${formattedPhone}&USER_PASSWORD=${password}&AUTH_FORM=Y&TYPE=AUTH&USER_REMEMBER=Y&backurl=/signin/`;
+        signInPath = signInPath
+            .replace('+', '%2B')
+            .replace('(', '+%28')
+            .replace(')', '%29+')
+            .replace('/', '%2F')
+            .replace('/', '%2F')
+
+
+        console.log(signInPath);
+
+        var signin = await request({
+            link: `https://evrasia.spb.ru/signin/`,
+            method: 'POST',
+            data: signInPath,
+            headers: {
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'ru',
+                'cache-control': 'max-age=0',
+                'content-type': 'application/x-www-form-urlencoded',
+                'Cookie': cookieString,
+                'dnt': '1',
+                'content-lenght': signInPath.length,
+                'origin': 'https://evrasia.spb.ru',
+                'referer': 'https://evrasia.spb.ru/signin/',
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'sec-gpc': '1',
+                'user-agent': userAgent,
+            }
+        });
+
+        if(signin.statusCode == 302 && signin.headers['set-cookie'] != undefined) {
+            return {
+                ok: true,
+                result: signin.headers['set-cookie'],
+            }
         }
 
-
-
-        setTimeout(async () => {
-            var formattedPhone = `+${login.substring(1, 2)}(${login.substring(2, 5)})${login.substring(5, 8)}-${login.substring(8, 10)}-${login.substring(10, 12)}`;
-            var signInPath = `USER_LOGIN=${formattedPhone}&USER_PASSWORD=${password}&AUTH_FORM=Y&TYPE=AUTH&USER_REMEMBER=Y&backurl=/signin/`;
-            signInPath = signInPath
-                .replace('+', '%2B')
-                .replace('(', '+%28')
-                .replace(')', '%29+')
-                .replace('/', '%2F')
-                .replace('/', '%2F')
-
-
-            console.log(signInPath);
-
-            var signin = await request({
-                link: `https://evrasia.spb.ru/signin/`,
-                method: 'POST',
-                data: signInPath,
-                headers: {
-                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                    'accept-encoding': 'gzip, deflate, br',
-                    'accept-language': 'ru',
-                    'cache-control': 'max-age=0',
-                    'content-type': 'application/x-www-form-urlencoded',
-                    'Cookie': cookieString,
-                    'dnt': '1',
-                    'content-lenght': signInPath.length, 
-                    'origin': 'https://evrasia.spb.ru',
-                    'referer': 'https://evrasia.spb.ru/signin/',
-                    'sec-fetch-dest': 'document',
-                    'sec-fetch-mode': 'navigate',
-                    'sec-fetch-site': 'same-origin',
-                    'sec-fetch-user': '?1',
-                    'upgrade-insecure-requests': '1',
-                    'sec-gpc': '1',
-                    'user-agent': userAgent,
-                }
-            });
-
-            //PHPSESSID=73b383501e1df364e4217a156e863675; BITRIX_SM_SALE_UID=148690771
-            //PHPSESSID=ca581222453d9237faae6b7ef0221d15; BITRIX_SM_SALE_UID=148688027
-
-            //+7 (911) 812-96-95
-            //USER_LOGIN=%2B7+%28911%29+812-96-95&USER_PASSWORD=Tosha812&AUTH_FORM=Y&TYPE=AUTH&USER_REMEMBER=Y&backurl=%2Fsignin%2F
-            //USER_LOGIN=%2B7+%28911%29+812-96-95&USER_PASSWORD=Tosha812&AUTH_FORM=Y&TYPE=AUTH&USER_REMEMBER=Y&backurl=%2Fsignin%2F
-            console.log(cookieString);
-            console.log(signin.statusCode);
-            console.log(signin.headers);
-            console.log(signin.body);
-            console.log(signin.requestHeader);
-
-        }, 1500);
-
-        /*var cookies: string[] = [
-            
-        ];
-        
-        var firstOpen_Cookies = await request({ 
-            link: 'https://evrasia.spb.ru/signin/',
-            headers: {
-                'user-agent': userAgent,
-            }
-        });
-
-        var sessionId = 
-
-        cookies = cookies.concat(firstOpen_Cookies.headers['set-cookie'][0].split(';')[0]);
-        cookies = cookies.concat(firstOpen_Cookies.headers['set-cookie'][1].split(';')[0])
-
-        cookies = cookies.concat('CHOOSED_RESTAURANT_EVERYTIME=true');
-
-       /* var userId_Cookie = await request({ 
-            link: 'https://bitrix.info/ba.js',
-            headers: {
-                'user-agent': userAgent,
-                Cookie: [cookies[0], cookies[1]].join('; '),
-            }
-        });
-
-        console.log(sessionId);
-        console.log([cookies[0], cookies[1]].join('; '));
-
-        var bx_user_id = userId_Cookie.headers['set-cookie'][0].split(';')[0];
-        bx_user_id = bx_user_id.split('=')[0].toUpperCase() + '=' + bx_user_id.split('=')[1]; 
-
-        cookies = cookies.concat(bx_user_id);
-
-        
-        //PHPSESSID=0b16ec87e3252226cc3cbe69236f52b9; BITRIX_SM_SALE_UID=148638106
-        
-        console.log([cookies[0], cookies[1]].join('; '));
-
-        
-        var BITRIX_CONVERSION_CONTEXT_s1 = await request({
-            method: 'POST',
-            link: `https://evrasia.spb.ru/bitrix/tools/conversion/ajax_counter.php?SITE_ID=s1&sessid=${sessionId}&HTTP_REFERER=`,
-            headers: {
-                Cookie: [cookies[0], cookies[1]].join('; '),
-                ContentType: 'application/x-www-form-urlencoded',
-                UserAgent: userAgent,
-            }
-        });
-
-        console.log(BITRIX_CONVERSION_CONTEXT_s1.request);
-        console.log('abobus');
-        console.log(BITRIX_CONVERSION_CONTEXT_s1.headers['set-cookie']);
-
-        cookies = cookies.concat('BITRIX_CONVERSION_CONTEXT_s1={"ID":7,"EXPIRE":1674680340,"UNIQUE":["conversion_visit_day"]}');
-
-        
-
-        var link = `https://evrasia.spb.ru/signin/?USER_LOGIN=${encodeURI(formattedPhone)}&USER_PASSWORD=${password}&AUTH_FORM=Y&TYPE=AUTH&USER_REMEMBER=Y&backurl=/signin/`;
-
-
-        /*
-        PHPSESSID=2b52884ed85d9621a194e57767fe1eeb; 
-        BITRIX_SM_SALE_UID=148634758; 
-        CHOOSED_RESTAURANT_EVERYTIME=true;  
-        BITRIX_CONVERSION_CONTEXT_s1={"ID":7,"EXPIRE":1674680340,"UNIQUE":["conversion_visit_day"]}; 
-        BX_USER_ID=ff74a86ae1369321fb6c42f3f18e7f1e; */
-
-        /**
-         * PHPSESSID=d810abfc637f34d72de8a62401709f37; BITRIX_SM_SALE_UID=148640062; CHOOSED_RESTAURANT_EVERYTIME=true; BX_USER_ID=80c95800d6c5ad7a835b465000ac0af7; BITRIX_CONVERSION_CONTEXT_s1={"ID":7,"EXPIRE":1674680340,"UNIQUE":["conversion_visit_day"]}
-         * PHPSESSID=0b16ec87e3252226cc3cbe69236f52b9; BITRIX_SM_SALE_UID=148638106; CHOOSED_RESTAURANT_EVERYTIME=true; BX_USER_ID=e54201a0c788c03f3679c864aeef4e63; BITRIX_CONVERSION_CONTEXT_s1={"ID":7,"EXPIRE":1674680340,"UNIQUE":["conversion_visit_day"]} 
-        */
-
-        /*
-                console.log(cookies.join('; '));
-        
-                var r = await request({
-                    link: link,
-                    method: 'POST',
-                    headers: {
-                        'content-type': 'application/x-www-form-urlencoded',
-                        'user-agent': userAgent,
-                        'cookie': cookies.join('; '),
-                    }
-                })
-        
-        
-        
-                var s = fs.createWriteStream('curr.html');
-                s.once('open', (_) => {
-                    s.write(r.data);
-                });
-        
-        
-        
-                console.log(r.status);
-        
-                //console.log(r.headers);
-        
-        
-                if (r.status == 303) {
-                    return {
-                        ok: true,
-                        result: r.headers['set-cookie'].join(';')
-                    }
-                }
-        */
         return {
             ok: false
         }
     }
 
-    static Logout(user: string) {
+    static Logout(user: user) {
 
     }
 
-    static GetCode(user: string): number {
+    static matchAll(regex, value): string[] {
+        var re = regex;
+        var s = value;
+        var m;
+
+        var r = [];
+
+        do {
+            m = re.exec(s);
+            console.log(m);
+            if (m) {
+                r.push(m[1]);
+            }
+        } while (m);
+
+        return r;
+    }
+
+    /*static async GetCode(user: user): Promise<RequestResult<number>> {
         return -1;
-    }
+    }*/
 
-    static GetCards(user: string) {
+    static GetCards(user: user) {
 
     }
 
@@ -276,12 +142,44 @@ export class EvrasiaApi {
         return -1;
     }
 
-    static GetAdresess(user: string): Array<RestaurantAdress> {
-        return [];
+    static async GetAdresess(user: user): Promise<RequestResult<RestaurantAdress[]>> {
+        var accountRequest = await request({
+            link: 'https://evrasia.spb.ru/account/',
+            headers: {
+                'user-agent': user.userAgent,
+                'cookie': this.glueCookie((JSON.parse(user.cookies) as string[]).map((e) => this.cutCookie(e)))
+            }
+        });
+
+        if(accountRequest.statusCode == 200) {
+            var m = /<option value="">Выберите ресторан<\/option>([.,\s,]*<option value=\"\d{1,}\">.*<\/option>)*/.exec(accountRequest.body)[0];
+            var adresess = this.matchAll(/(<option value=\"\d{1,}\">.*<\/option>)/g, m);
+
+            var result = adresess.map((e) => {
+                var code = /<option value=\"(\d*)\">/.exec(e)[1]
+                var name = />(.*)<\/option>/.exec(e)[1];
+
+                var d: RestaurantAdress = {
+                    name: name,
+                    index: parseInt(code),
+                }
+
+                return d;
+            });
+
+            return {
+                ok: true,
+                result: result,
+            } 
+        }
+    
+        return {
+            ok: false
+        }
     }
 }
 
-interface RestaurantAdress {
+export interface RestaurantAdress {
     name: string;
     index: number;
 }
